1e6b9099198ba756ba37fb36503e724e23575f11
SG-0205 <samgold0205@gmail.com>
2025-01-25 19:10:10 +0100

    Fixed player on borders

--- src/map_closure_check.c
+++ src/map_closure_check.c
@@ -55,8 +55,8 @@ static int	is_supported_char(char *to_test)
 	char	value;
 
 	value = *to_test;
-	if (value == ' ' || value == '\t' || value == '\r' || value == 'N'
-		|| value == 'S' || value == 'E' || value == 'W' || value == '1')
+	if (value == ' ' || value == '\t' || value == '\r' || value == 'N' ||
+		value == 'S' || value == 'E' || value == 'W' || value == '1')
 		return (1);
 	return (0);
 }
@@ -70,8 +70,9 @@ int	floodfill(t_gm *gm, int x, int y, int *max)
 
 	if (!gm)
 		return (-1);
-	if ((((x > max[0] || !gm->map[x][y]) && gm->map[x - 1][y] != '1')
-			|| ((y > max[1] || !gm->map[x][y]) && gm->map[x][y - 1] != '1')))
+	if ((x < 0 || y < 0) ||
+		(((x > max[0] || !gm->map[x][y]) && gm->map[x - 1][y] != '1') ||
+			((y > max[1] || !gm->map[x][y]) && gm->map[x][y - 1] != '1')))
 		ft_error(gm, "Map isn't fully bordered by walls.\n");
 	if (is_supported_char(&gm->map[x][y]))
 		return (1);
@@ -101,7 +102,7 @@ void	map_closure_check(t_gm *gm, char *file)
 	{
 		while (gm->map[x][++y])
 			if (gm->map[x][y] == '0')
-				floodfill(gm, x, y, (int [2]){xmax, ymax});
+				floodfill(gm, x, y, (int[2]){xmax, ymax});
 		y = -1;
 	}
 	x = y;
